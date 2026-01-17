9ece6eb466bf1da605e1ad8e65ac750702a53230
Ivan Platunov <i@iplatunov.ru>
2026-01-17 23:16:54 +0300

    First Commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia' 
+import App from './App.vue'
+import router from './router'
+import Axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
